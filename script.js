81a7f7ca06f0a0d9e8d5fbe737e8e7fbfc90dd7c
dikshayadav15122010-lgtm <dikshayadav15122010@gmail.com>
2026-07-07 13:09:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", function(){
+
+document.body.innerHTML=`
+
+<div class="container">
+
+<h1>Hello :)</h1>
+
+<p>This is where our letter will begin...</p>
+
+</div>
+
+`;
+
+});
